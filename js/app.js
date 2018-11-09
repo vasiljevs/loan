288944bfa352f30a21d8741b54e3abb457613c17
Arturs Vasiljevs <freelancerarthur@gmail.com>
2018-11-09 10:33:53 +0200

    Delete - most of the functionality

--- js/app.js
+++ js/app.js
@@ -1,60 +1,29 @@
-const form = document.querySelector('#loan-form'),
-amount = document.querySelector('#amount'),
-interest = document.querySelector('#interest'),
-years = document.querySelector('#years'),
-monthly = document.querySelector('#monthly-payment'),
-totalPayment = document.querySelector('#total-payment'),
-totalInterest = document.querySelector('#total-interest');
+const form = document.querySelector('form');
 
-form.addEventListener('submit', (e) => {
-  document.querySelector('#results').style.display = 'none';
-  document.querySelector('#loading').style.display = 'block';
+// Input fields
+const amount = document.querySelector('#amount'),
+    interest = document.querySelector('#interest'),
+      period = document.querySelector('#period');
 
-  setTimeout(calculateResults, 2000);
+function getLoanResult () {
+  // GET  values
+  const principal = parseFloat(amount.value),
+    loanInterest = parseFloat(interest.value) / 100 / 12;
+    loanPayment = parseFloat(period.value) * 12;
 
-  e.preventDefault();
-});
+  // Calculate monthly payment
+  const calculate = Math.pow(1 + loanInterest, loanPayment),
+    calculateMonthly = (principal * calculate * loanInterest) / (calculate - 1);
 
-function calculateResults() {
-  const principal = parseFloat(amount.value);
-  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
-  const calculatedPayments = parseFloat(years.value) * 12;
-
-  // Monthly payment
-  const calc = Math.pow(1 + calculatedInterest, calculatedPayments);
-  const calcMonthly = (principal * calc * calculatedInterest) / (calc - 1);
-
-  if (isFinite(calcMonthly)) {
-    monthly.value = calcMonthly.toFixed(2);
-    totalPayment.value = (calcMonthly * calculatedPayments).toFixed(2);
-    totalInterest.value = ((calcMonthly * calculatedPayments) - principal).toFixed(2);
-    // Show 
-    document.querySelector('#results').style.display = 'block';
-    // Hide
-    document.querySelector('#loading').style.display = 'none';
-  } else {
-    showError('Please check your numbers!');
+  // Check if the supplied number is finite
+  if (isFinite(calculateMonthly)) {
+    console.log(`Monthly: ${calculateMonthly.toFixed(2)} USD`);
+    console.log(`Interest: ${((calculateMonthly * loanPayment) - principal).toFixed(2)} USD`);
+    console.log(`Total: ${(calculateMonthly * loanPayment).toFixed(2)} USD`);
   }
 }
 
-function showError(err) {
-  // Show 
-  document.querySelector('#results').style.display = 'none';
-    
-  // Hide
-  document.querySelector('#loading').style.display = 'none';
-  
-  const div = document.createElement('div');
-
-  const card = document.querySelector('.card'),
-  heading = document.querySelector('.heading');
-
-  div.className = 'alert alert-danger';
-  div.appendChild(document.createTextNode(err));
-
-  card.insertBefore(div, heading);
-
-  setTimeout(() => {
-    document.querySelector('.alert').remove();
-  }, 3000);
-}
+form.addEventListener('submit', (e) => {
+  setTimeout(getLoanResult, 3000);
+  e.preventDefault();
+});
