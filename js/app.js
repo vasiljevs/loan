d4488304792b43ae4330126360633e1e5479a016
Arturs Vasiljevs <freelancerarthur@gmail.com>
2018-12-13 13:30:45 +0200

    Add - basic functionality

--- js/app.js
+++ js/app.js
@@ -1,29 +1,64 @@
-const form = document.querySelector('form');
+'use strict';
 
-// Input fields
-const amount = document.querySelector('#amount'),
-    interest = document.querySelector('#interest'),
-      period = document.querySelector('#period');
+const amount = document.querySelector('.payment-amount'),
+    interest = document.querySelector('.payment-interest'),
+      period = document.querySelector('.payment-period');
 
-function getLoanResult () {
+const setHeading = name => {
+  const heading = document.querySelector('.heading');
+  heading.textContent = name;
+};
+
+const setOverlayClass = (stClass, ndClass) => {
+  const overlay = document.querySelector('.overlay');
+  overlay.classList.replace(stClass, ndClass);
+};
+
+const loan = {
+  getPrincipal: function () {
+    return parseFloat(amount.value);
+  },
+  getInterest: function () {
+    return parseFloat(interest.value) / 100 / 12;
+  },
+  getPeriod: function () {
+    return parseFloat(period.value) * 12;
+  }
+};
+
+document.querySelector('.application').addEventListener('submit', (e) => {
+  setOverlayClass('d-none', 'd-block');
+  setHeading('Results');
+
   // GET  values
-  const principal = parseFloat(amount.value),
-    loanInterest = parseFloat(interest.value) / 100 / 12;
-    loanPayment = parseFloat(period.value) * 12;
+  const principal = loan.getPrincipal();
+  const loanInterest = loan.getInterest();
+  const loanPayment = loan.getPeriod();
 
   // Calculate monthly payment
-  const calculate = Math.pow(1 + loanInterest, loanPayment),
-    calculateMonthly = (principal * calculate * loanInterest) / (calculate - 1);
+  const calculate = Math.pow(1 + loanInterest, loanPayment);
+  const calculateMonthly = (principal * calculate * loanInterest) / (calculate - 1);
 
   // Check if the supplied number is finite
   if (isFinite(calculateMonthly)) {
-    console.log(`Monthly: ${calculateMonthly.toFixed(2)} USD`);
-    console.log(`Interest: ${((calculateMonthly * loanPayment) - principal).toFixed(2)} USD`);
-    console.log(`Total: ${(calculateMonthly * loanPayment).toFixed(2)} USD`);
-  }
-}
+    const calcMonthly = calculateMonthly.toFixed(2);
+    const calcInterest = ((calculateMonthly * loanPayment) - principal).toFixed(2);
+    const calcTotal = (calculateMonthly * loanPayment).toFixed(2);
 
-form.addEventListener('submit', (e) => {
-  setTimeout(getLoanResult, 3000);
+    const monthly = document.querySelector('.monthly');
+    monthly.textContent = `${calcMonthly} USD`;
+
+    const interest = document.querySelector('.interest');
+    interest.textContent = `${calcInterest} USD`;
+
+    const total = document.querySelector('.total');
+    total.textContent = `${calcTotal} USD`;
+  }
+
   e.preventDefault();
+});
+
+document.querySelector('.overlay button').addEventListener('click', () => {
+  setOverlayClass('d-block', 'd-none');
+  setHeading('Loan Calculator');
 });
